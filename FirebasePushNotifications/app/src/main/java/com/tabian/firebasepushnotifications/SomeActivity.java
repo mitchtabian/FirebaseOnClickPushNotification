c5afdc5fa2a3aff6278f494c8e3aee06472e198f
Mitch Tabian <mitch@tabian.ca>
2017-02-23 17:04:58 -0800

    Source Code

---
+++ FirebasePushNotifications/app/src/main/java/com/tabian/firebasepushnotifications/SomeActivity.java
@@ -0,0 +1,18 @@
+package com.tabian.firebasepushnotifications;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by User on 2/23/2017.
+ */
+
+
+public class SomeActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.some_activity_layout);
+    }
+}
